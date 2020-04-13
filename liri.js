f774caed8f0f9b09c750c28c1198a883419ee9f4
Jared DePalma <depalmajared91@gmail.com>
2020-04-12 18:30:04 -0600

    small edits

--- liri.js
+++ liri.js
@@ -1,3 +1,9 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+let inquirer = require("inquirer");
+let moment = require("moment");
+moment().format();
+let fs = require("fs");
+let axios = require("axios");
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
